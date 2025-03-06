bd147ea4aabfbec53f5c5257a67beac7fe704cc4
Durga Prasad <akuladurgaprasad5@gmail.com>
2025-03-06 12:18:59 +0530

    CrossOrigin function Added

--- src/main/java/com/example/contactapi/resource/ContactResource.java
+++ src/main/java/com/example/contactapi/resource/ContactResource.java
@@ -5,14 +5,7 @@ import com.example.contactapi.service.ContactService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -26,6 +19,7 @@ import static org.springframework.http.MediaType.IMAGE_PNG_VALUE;
 
 @RestController
 @RequestMapping("/contacts")
+@CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 public class ContactResource {
     private final ContactService contactService;
